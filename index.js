0f14eb1e9afea3904ec4b20c1e34448612da2305
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2021-11-30 17:04:41 -0800

    express
    if applied this will make changes to the original node.js modules to have express instead

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-ECHO is on.
+const express = require("express");
+const app = express();
+const morgan = require("morgan")
+
+let topMovies = [
+{
+ title: 'harry potter and the sorcerer\'s stone',
+ author: 'J.K. Rowling'
+},
+{
+ title: 'Lord of the rings',
+ author:'J.R.R. Tolkien'
+},
+{
+ title: 'Twilight',
+ author: 'stephanie Meyer'
+
+ }
+];
+
+app.use(morgan('common'));
+
+
+// GET requests \\
+app.get('/', (req, res) => {
+res.send('Welcome to my app');
+});
+
+app.get('/secreturl', (req, res) => {
+res.send('this is a secret url with top secret content!')
+});
+
+app.get('/movies', (req, res) => {
+res.json(topMovies);
+});
+
+app.use('/documentation.html', express.static('public'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+// listen for requests \\
+app.listen(8080, () => {
+ console.log('your app is listening on port 8080.')
+});
+
