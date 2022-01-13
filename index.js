785279afa764b364c7a07a0e852e38d42cac3de9
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2022-01-12 16:48:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
   //true});
   mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology:
   true});
-
+  access-control-allow-Origins
 
   // this code only allows certain access to certain origins to get info from the API
   
@@ -22,18 +22,20 @@
   
   app.use(morgan("common"));
   app.use(bodyParser.json());
-  //const cors = require('cors');
-  //app.use(cors({
-    //origin: (origin, callback) => {
-   // if(!origin) return callback(null, true);
-   // if(allowedOrigins.indexOf(origin) === -1){
-   // let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-   // return callback(new Error(message ), false);
-   // }
-   // return callback(null, true);
-    //}
-  //}));
-  let allowedOrigins = ['http:localhost:1234', 'http:localhost:8080'];
+  const cors = require('cors');
+  app.use(cors());
+  /*app.use(cors({
+    origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+    let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+    return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+    }
+  }));
+  
+  let allowedOrigins = ['http:localhost:1234', 'http:localhost:8080'];*/
   let auth = require('./auth.js')(app);
   const passport = require('passport');
   require('./passport');
