8737c55af3d99b8b22b98b41b709757d3c75c9fd
bailey P Mashore <90733291+Baileym8203@users.noreply.github.com>
2022-01-12 16:32:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -22,15 +22,15 @@
   
   app.use(morgan("common"));
   app.use(bodyParser.json());
-  const cors = require('cors');
-  app.use(cors({
-    origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-    let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-    return callback(new Error(message ), false);
-    }
-    return callback(null, true);
+  //const cors = require('cors');
+  //app.use(cors({
+    //origin: (origin, callback) => {
+   // if(!origin) return callback(null, true);
+   // if(allowedOrigins.indexOf(origin) === -1){
+   // let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+   // return callback(new Error(message ), false);
+   // }
+   // return callback(null, true);
     }
   }));
   let allowedOrigins = ['http:localhost:1234', 'http:localhost:8080'];
